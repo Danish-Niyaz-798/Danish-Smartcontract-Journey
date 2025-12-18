8b8d9b827c3c71ea3847d3605314a0b5f6384629
Mohd Danish Niyaz <Danishniyaz786@gmail.com>
2025-12-18 10:31:46 +0530

    Update StorageFactory.sol

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.18;
+
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage[] public listOfSimpleStorageContarcts;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContarcts.push(newSimpleStorageContract);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        //Address
+        // ABI -Application Binary Interaface
+        SimpleStorage mySimpleStorage = SimpleStorage(listOfSimpleStorageContarcts[_simpleStorageIndex]);
+        mySimpleStorage.store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContarcts[_simpleStorageIndex];
+        return mySimpleStorage.retrieve();
+    } 
+}
